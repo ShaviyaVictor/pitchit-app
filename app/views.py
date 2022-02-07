74a33b53a95912d0e584125daf788a945220e8f2
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-07 05:44:55 +0300

    Add a new route

--- app/views.py
+++ app/views.py
@@ -10,3 +10,11 @@ def index() :
   View root page function that returns the index page and its data
   '''
   return render_template('index.html')
+
+
+
+@app.route('/pitches')
+def pitch() :
+  '''
+  View pitch page function that returns the pitches available
+  '''
