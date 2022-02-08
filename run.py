d2f417ac57814a7288440d020b30810d9fd1a803
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-08 13:41:05 +0300

    Fix the compatibility issue and enable the support for writing external scripts in Flask by installing the Flask-Script extension

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 # File that will run our application
 
-from cProfile import run
+# from cProfile import run
+# from multiprocessing import Manager
+from flask_script import Manager
 from app import app, db
 
+
+manager = Manager(app)
 
 
 from app.models import user
@@ -11,11 +15,11 @@ from app.models import user
 
 
 
-
-@run.shell
+# a Python shell that allows us to pass in some properties into our shell
+@manager.shell
 def make_shell_context() :
   return dict(app = app, db = db, user = user)
 
 
 if __name__ == '__main__' :
-  app.run()
+  manager.run()
